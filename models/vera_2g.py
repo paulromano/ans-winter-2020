8be283e8198951de1755f1ec7f887efdf60287cd
Paul Romano <paul.k.romano@gmail.com>
2020-06-26 09:16:42 -0500

    Add VERA 2g model

--- models/vera_2g.py
+++ models/vera_2g.py
@@ -58,6 +58,43 @@ water_600.add_nuclide('B10', 1.00874E-05)
 water_600.add_nuclide('B11', 4.06030E-05)
 water_600.add_s_alpha_beta('c_H_in_H2O')
 
+ss304 = openmc.Material(name='SS304')
+ss304.set_density('atom/b-cm', 8.82490E-02)
+ss304.add_element('C', 3.20895E-04)
+ss304.add_nuclide('Si28', 1.58197E-03)
+ss304.add_nuclide('Si29', 8.03653E-05)
+ss304.add_nuclide('Si30', 5.30394E-05)
+ss304.add_nuclide('P31', 6.99938E-05)
+ss304.add_nuclide('Cr50', 7.64915E-04)
+ss304.add_nuclide('Cr52', 1.47506E-02)
+ss304.add_nuclide('Cr53', 1.67260E-03)
+ss304.add_nuclide('Cr54', 4.16346E-04)
+ss304.add_nuclide('Mn55', 1.75387E-03)
+ss304.add_nuclide('Fe54', 3.44776E-03)
+ss304.add_nuclide('Fe56', 5.41225E-02)
+ss304.add_nuclide('Fe57', 1.24992E-03)
+ss304.add_nuclide('Fe58', 1.66342E-04)
+ss304.add_nuclide('Ni58', 5.30854E-03)
+ss304.add_nuclide('Ni60', 2.04484E-03)
+ss304.add_nuclide('Ni61', 8.88879E-05)
+ss304.add_nuclide('Ni62', 2.83413E-04)
+ss304.add_nuclide('Ni64', 7.21770E-05)
+
+agincd= openmc.Material(name='Ag-In-Cd')
+agincd.set_density('atom/b-cm', 5.63131E-02)
+agincd.add_nuclide('Ag107', 2.36159E-02)
+agincd.add_nuclide('Ag109', 2.19403E-02)
+agincd.add_nuclide('Cd106', 3.41523E-05)
+agincd.add_nuclide('Cd108', 2.43165E-05)
+agincd.add_nuclide('Cd110', 3.41250E-04)
+agincd.add_nuclide('Cd111', 3.49720E-04)
+agincd.add_nuclide('Cd112', 6.59276E-04)
+agincd.add_nuclide('Cd113', 3.33873E-04)
+agincd.add_nuclide('Cd114', 7.84957E-04)
+agincd.add_nuclide('Cd116', 2.04641E-04)
+agincd.add_nuclide('In113', 3.44262E-04)
+agincd.add_nuclide('In115', 7.68050E-03)
+
 ###############################################################################
 # Geometry
 
@@ -67,6 +104,9 @@ clad_ir = openmc.ZCylinder(r=0.4180, name='Clad IR')
 clad_or = openmc.ZCylinder(r=0.4750, name='Clad OR')
 gt_ir   = openmc.ZCylinder(r=0.5610, name='Guide Tube IR')
 gt_or   = openmc.ZCylinder(r=0.6020, name='Guide Tube OR')
+aic_ir      = openmc.ZCylinder(r=0.3820, name='AIC Radius')
+aic_clad_ir = openmc.ZCylinder(r=0.3860, name='AIC Clad IR')
+aic_clad_or = openmc.ZCylinder(r=0.4840, name='AIC Clad OR')
 
 assembly_left   = openmc.XPlane(x0=-10.75, boundary_type='reflective')
 assembly_right  = openmc.XPlane(x0= 10.75, boundary_type='reflective')
@@ -88,32 +128,38 @@ u_fuel = openmc.model.pin([fuel_or, clad_ir, clad_or], [fuel_31, helium, zirc4, 
 # guide tube
 u_gt = openmc.model.pin([gt_ir, gt_or], [water_600, zirc4, water_600])
 
-# Instantiate a Lattice
-lattice_2a = openmc.RectLattice()
-lattice_2a.lower_left = [-10.71, -10.71]
-lattice_2a.pitch      = [1.2600, 1.2600]
+
+# Ag-In-Cd (AIC) control rod
+u_aic = openmc.model.pin(
+    surfaces=[aic_ir, aic_clad_ir, aic_clad_or, gt_ir, gt_or],
+    items=[agincd, helium, ss304, water_600, zirc4, water_600]
+)
+
+lattice_2g = openmc.RectLattice(lattice_id=4)
+lattice_2g.lower_left = [-10.71, -10.71]
+lattice_2g.pitch      = [1.2600, 1.2600]
                           # 1        2       3      4       5        6       7       8       9       10     11       12      13     14      15      16      17
-lattice_2a.universes  = [[u_fuel, u_fuel, u_fuel, u_fuel, u_fuel, u_fuel, u_fuel, u_fuel, u_fuel, u_fuel, u_fuel, u_fuel, u_fuel, u_fuel, u_fuel, u_fuel, u_fuel], # 1
-                         [u_fuel, u_fuel, u_fuel, u_fuel, u_fuel, u_fuel, u_fuel, u_fuel, u_fuel, u_fuel, u_fuel, u_fuel, u_fuel, u_fuel, u_fuel, u_fuel, u_fuel], # 2
-                         [u_fuel, u_fuel, u_fuel, u_fuel, u_fuel, u_gt  , u_fuel, u_fuel, u_gt  , u_fuel, u_fuel, u_gt  , u_fuel, u_fuel, u_fuel, u_fuel, u_fuel], # 3
-                         [u_fuel, u_fuel, u_fuel, u_gt  , u_fuel, u_fuel, u_fuel, u_fuel, u_fuel, u_fuel, u_fuel, u_fuel, u_fuel, u_gt  , u_fuel, u_fuel, u_fuel], # 4
-                         [u_fuel, u_fuel, u_fuel, u_fuel, u_fuel, u_fuel, u_fuel, u_fuel, u_fuel, u_fuel, u_fuel, u_fuel, u_fuel, u_fuel, u_fuel, u_fuel, u_fuel], # 5
-                         [u_fuel, u_fuel, u_gt  , u_fuel, u_fuel, u_gt  , u_fuel, u_fuel, u_gt  , u_fuel, u_fuel, u_gt  , u_fuel, u_fuel, u_gt  , u_fuel, u_fuel], # 6
-                         [u_fuel, u_fuel, u_fuel, u_fuel, u_fuel, u_fuel, u_fuel, u_fuel, u_fuel, u_fuel, u_fuel, u_fuel, u_fuel, u_fuel, u_fuel, u_fuel, u_fuel], # 7
-                         [u_fuel, u_fuel, u_fuel, u_fuel, u_fuel, u_fuel, u_fuel, u_fuel, u_fuel, u_fuel, u_fuel, u_fuel, u_fuel, u_fuel, u_fuel, u_fuel, u_fuel], # 8
-                         [u_fuel, u_fuel, u_gt  , u_fuel, u_fuel, u_gt  , u_fuel, u_fuel, u_gt  , u_fuel, u_fuel, u_gt  , u_fuel, u_fuel, u_gt  , u_fuel, u_fuel], # 9
-                         [u_fuel, u_fuel, u_fuel, u_fuel, u_fuel, u_fuel, u_fuel, u_fuel, u_fuel, u_fuel, u_fuel, u_fuel, u_fuel, u_fuel, u_fuel, u_fuel, u_fuel], # 10
-                         [u_fuel, u_fuel, u_fuel, u_fuel, u_fuel, u_fuel, u_fuel, u_fuel, u_fuel, u_fuel, u_fuel, u_fuel, u_fuel, u_fuel, u_fuel, u_fuel, u_fuel], # 11
-                         [u_fuel, u_fuel, u_gt  , u_fuel, u_fuel, u_gt  , u_fuel, u_fuel, u_gt  , u_fuel, u_fuel, u_gt  , u_fuel, u_fuel, u_gt  , u_fuel, u_fuel], # 12
-                         [u_fuel, u_fuel, u_fuel, u_fuel, u_fuel, u_fuel, u_fuel, u_fuel, u_fuel, u_fuel, u_fuel, u_fuel, u_fuel, u_fuel, u_fuel, u_fuel, u_fuel], # 13
-                         [u_fuel, u_fuel, u_fuel, u_gt  , u_fuel, u_fuel, u_fuel, u_fuel, u_fuel, u_fuel, u_fuel, u_fuel, u_fuel, u_gt  , u_fuel, u_fuel, u_fuel], # 14
-                         [u_fuel, u_fuel, u_fuel, u_fuel, u_fuel, u_gt  , u_fuel, u_fuel, u_gt  , u_fuel, u_fuel, u_gt  , u_fuel, u_fuel, u_fuel, u_fuel, u_fuel], # 15
-                         [u_fuel, u_fuel, u_fuel, u_fuel, u_fuel, u_fuel, u_fuel, u_fuel, u_fuel, u_fuel, u_fuel, u_fuel, u_fuel, u_fuel, u_fuel, u_fuel, u_fuel], # 16
-                         [u_fuel, u_fuel, u_fuel, u_fuel, u_fuel, u_fuel, u_fuel, u_fuel, u_fuel, u_fuel, u_fuel, u_fuel, u_fuel, u_fuel, u_fuel, u_fuel, u_fuel]] # 17
+lattice_2g.universes  = [ [u_fuel, u_fuel, u_fuel, u_fuel, u_fuel, u_fuel, u_fuel, u_fuel, u_fuel, u_fuel, u_fuel, u_fuel, u_fuel, u_fuel, u_fuel, u_fuel, u_fuel], # 1
+                          [u_fuel, u_fuel, u_fuel, u_fuel, u_fuel, u_fuel, u_fuel, u_fuel, u_fuel, u_fuel, u_fuel, u_fuel, u_fuel, u_fuel, u_fuel, u_fuel, u_fuel], # 2
+                          [u_fuel, u_fuel, u_fuel, u_fuel, u_fuel, u_aic , u_fuel, u_fuel, u_aic , u_fuel, u_fuel, u_aic , u_fuel, u_fuel, u_fuel, u_fuel, u_fuel], # 3
+                          [u_fuel, u_fuel, u_fuel, u_aic , u_fuel, u_fuel, u_fuel, u_fuel, u_fuel, u_fuel, u_fuel, u_fuel, u_fuel, u_aic , u_fuel, u_fuel, u_fuel], # 4
+                          [u_fuel, u_fuel, u_fuel, u_fuel, u_fuel, u_fuel, u_fuel, u_fuel, u_fuel, u_fuel, u_fuel, u_fuel, u_fuel, u_fuel, u_fuel, u_fuel, u_fuel], # 5
+                          [u_fuel, u_fuel, u_aic , u_fuel, u_fuel, u_aic , u_fuel, u_fuel, u_aic , u_fuel, u_fuel, u_aic , u_fuel, u_fuel, u_aic , u_fuel, u_fuel], # 6
+                          [u_fuel, u_fuel, u_fuel, u_fuel, u_fuel, u_fuel, u_fuel, u_fuel, u_fuel, u_fuel, u_fuel, u_fuel, u_fuel, u_fuel, u_fuel, u_fuel, u_fuel], # 7
+                          [u_fuel, u_fuel, u_fuel, u_fuel, u_fuel, u_fuel, u_fuel, u_fuel, u_fuel, u_fuel, u_fuel, u_fuel, u_fuel, u_fuel, u_fuel, u_fuel, u_fuel], # 8
+                          [u_fuel, u_fuel, u_aic , u_fuel, u_fuel, u_aic , u_fuel, u_fuel, u_gt  , u_fuel, u_fuel, u_aic , u_fuel, u_fuel, u_aic , u_fuel, u_fuel], # 9
+                          [u_fuel, u_fuel, u_fuel, u_fuel, u_fuel, u_fuel, u_fuel, u_fuel, u_fuel, u_fuel, u_fuel, u_fuel, u_fuel, u_fuel, u_fuel, u_fuel, u_fuel], # 10
+                          [u_fuel, u_fuel, u_fuel, u_fuel, u_fuel, u_fuel, u_fuel, u_fuel, u_fuel, u_fuel, u_fuel, u_fuel, u_fuel, u_fuel, u_fuel, u_fuel, u_fuel], # 11
+                          [u_fuel, u_fuel, u_aic , u_fuel, u_fuel, u_aic , u_fuel, u_fuel, u_aic , u_fuel, u_fuel, u_aic , u_fuel, u_fuel, u_aic , u_fuel, u_fuel], # 12
+                          [u_fuel, u_fuel, u_fuel, u_fuel, u_fuel, u_fuel, u_fuel, u_fuel, u_fuel, u_fuel, u_fuel, u_fuel, u_fuel, u_fuel, u_fuel, u_fuel, u_fuel], # 13
+                          [u_fuel, u_fuel, u_fuel, u_aic , u_fuel, u_fuel, u_fuel, u_fuel, u_fuel, u_fuel, u_fuel, u_fuel, u_fuel, u_aic , u_fuel, u_fuel, u_fuel], # 14
+                          [u_fuel, u_fuel, u_fuel, u_fuel, u_fuel, u_aic , u_fuel, u_fuel, u_aic , u_fuel, u_fuel, u_aic , u_fuel, u_fuel, u_fuel, u_fuel, u_fuel], # 15
+                          [u_fuel, u_fuel, u_fuel, u_fuel, u_fuel, u_fuel, u_fuel, u_fuel, u_fuel, u_fuel, u_fuel, u_fuel, u_fuel, u_fuel, u_fuel, u_fuel, u_fuel], # 16
+                          [u_fuel, u_fuel, u_fuel, u_fuel, u_fuel, u_fuel, u_fuel, u_fuel, u_fuel, u_fuel, u_fuel, u_fuel, u_fuel, u_fuel, u_fuel, u_fuel, u_fuel] ] # 17
 
 lat_inner = openmc.Cell(
     name='assembly inside',
-    fill=lattice_2a,
+    fill=lattice_2g,
     region=+lattice_left  & -lattice_right  & +lattice_back  & -lattice_front
 )
 lat_outer = openmc.Cell(
@@ -168,11 +214,15 @@ model.settings.entropy_mesh = entropy_mesh
 # Get heating in a neutron-only calculation
 tally = openmc.Tally(name='heating')
 tally.filters = [
-    openmc.MaterialFilter([fuel_31, helium, zirc4, water_600]),
+    openmc.MaterialFilter([fuel_31, helium, zirc4, ss304, agincd, water_600]),
     openmc.ParticleFilter(['neutron', 'photon', 'electron', 'positron'])
 ]
 tally.scores = ['fission', 'heating-local']
-model.tallies = openmc.Tallies([tally])
+
+total_tally = openmc.Tally(name='total')
+total_tally.scores = ['fission', 'heating-local', 'heating']
+
+model.tallies = openmc.Tallies([tally, total_tally])
 
 sp_path = model.run()
 with openmc.StatePoint(sp_path) as sp:
@@ -181,7 +231,7 @@ with openmc.StatePoint(sp_path) as sp:
 
 # Get heating in a coupled neutron-photon calculation
 model.settings.photon_transport = True
-model.settings.delayed_photon_scaling = True
+model.settings.delayed_photon_scaling = False
 model.tallies[0].scores = ['fission', 'heating']
 sp_path = model.run()
 with openmc.StatePoint(sp_path) as sp:
@@ -208,3 +258,4 @@ heating = df_neutron_photon[df_neutron_photon.score == 'heating'].copy()
 heating['percent'] = heating['mean'] / heating['mean'].sum()
 print(heating.to_string())
 print(heating.groupby('material').sum())
+
